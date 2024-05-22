0760206debdf9c89fc019ef02cfcaa7f5c8454c6
PiKaChuuuuu12345 <elmyrza.musabecov310503@gmail.com>
2024-05-22 15:00:25 +0300

    init

---
+++ src/app/constants/config.ts
@@ -0,0 +1,12 @@
+export const LOGO_URL =
+  'https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png';
+
+export const BG_IMG_URL =
+  'https://assets.nflxext.com/ffe/siteui/vlv3/41c789f0-7df5-4219-94c6-c66fe500590a/746f624b-2122-4ac5-b35f-5c8e8a8a5bc8/KG-ru-20240513-popsignuptwoweeks-perspective_alpha_website_large.jpg';
+
+export const tmdbConfig = {
+  imagePath: 'http://image.tmdb.org/t/p/w500/',
+  apiKey: '0160f158297b9a120891dd018314940a',
+  accessToken:
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMTYwZjE1ODI5N2I5YTEyMDg5MWRkMDE4MzE0OTQwYSIsInN1YiI6IjY2NGM5NDA3MjJlNGU5OTJlOThhMDc5YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PrymuL4i3D7qeUEJlBuBoVFu49biHB3MYr-ArbaRgBQ',
+};
